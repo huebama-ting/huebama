9cee263c5d11498bad3a45ed1cdc9b5b9a43f206
Jacky Liang <jliang06@uoguelph.ca>
2020-08-07 00:32:26 -0400

    Centred modal text for work term skills

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -10,7 +10,7 @@ import { NgbModal, NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-boot
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
-    <div class="modal-body">
+    <div class="modal-body text-center">
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Interteam communication<br>Oftentimes, communication to and from other teams was needed due to the multifaceted nature of the product to make sure issues were escalated to the right people or to confirm whether an issue was a feature or a bug; the communication aspect was learned from class during collaborative projects, with tweaks coming from experience on the job to better adjust to company environment</li>
         <li class="list-group-item">Knowledge of the testing workflow<br>A large portion of the job focussed on testing a product; therefore knowledge of how to conduct tests properly by covering edge cases, making a note of input and output, etc. was essential. Like above, the theoretical portion of the skill came from learning about testing in class, with experience on the job in the form of detailed Jira test cases and feedback for teammates allowing the task to be streamlined.</li>
