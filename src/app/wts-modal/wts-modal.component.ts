f22becdf4943014e510b731f5e81e816aa54c5f4
Jacky Liang <jliang06@uoguelph.ca>
2021-01-14 21:16:05 -0500

    Fix typos

--- src/app/wts-modal/wts-modal.component.ts
+++ src/app/wts-modal/wts-modal.component.ts
@@ -28,7 +28,7 @@ import { NgbModal, NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-boot
             <small class="text-muted">Skill #2</small>
           </div>
           <p class="content">The everyday of the job consisted of taking tasks off the ready pile, completing the task, and then merging the completed work back into the master branch in source control. As such, it was essential to learn how to write commit messages and pull request descriptions.</p>
-          <p class="text-muted outcome">While I have written commit messages before, this job was the first time I had to follow a strict guideline for them. That combined with the fact that I had never used pull requests in source control before meant that this skill was definitely learned on the job.</p>
+          <p class="text-muted outcome">While I have written commit messages before, this job was the first time I had to follow a strict guideline for them. That, combined with the fact that I had never used pull requests in source control before meant that this skill was definitely learned on the job.</p>
         </div>
 
         <div class="list-group-item flex-column align-items-start">
@@ -37,7 +37,7 @@ import { NgbModal, NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-boot
             <small class="text-muted">Skill #3</small>
           </div>
           <p class="content">The company has a monthly internal demo to introduce new features of the product to employees. Since I worked on a new feature, I was given the opportunity to take the wheel and demo the feature I coded.</p>
-          <p class="text-muted outcome">I had done a demo before as part of the Software Design II course, so this skill consisted applying what I had learned and experienced before with new contextual info I had on the job to improve the demo as much as possible.</p>
+          <p class="text-muted outcome">I had done a demo before as part of the Software Design II course, so this skill consisted of applying what I had learned and experienced before with new contextual info that I had on the job to improve the demo as much as possible.</p>
         </div>
       </div>
     </div>
